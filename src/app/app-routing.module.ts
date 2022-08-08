3ec444e060479473bd299fbd450e5ba38f144768
Angela <angelahb1991@hotmail.com>
2022-08-08 00:27:34 -0400

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarComponent } from './components/listar/listar.component';
+import { AgregarComponent } from './components/agregar/agregar.component';
+import { EditarComponent } from './components/editar/editar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', component: ListarComponent },
+{ path: 'agregar', component: AgregarComponent },
+{ path: 'editar/:id', component: EditarComponent },
+{ path: '**', component: ListarComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './components/listar/listar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AgregarComponent } from './components/agregar/agregar.component';
+import { EditarComponent } from './components/editar/editar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarComponent,
+    EditarComponent,
+    ListarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
